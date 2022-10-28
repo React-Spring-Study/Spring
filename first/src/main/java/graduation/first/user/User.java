e7cb2e8e291bbf53e1f7162fae70a320912940dc
NamJihyun99 <namjihyun9936@gmail.com>
2022-10-28 15:26:58 +0900

    fix:> add getter in user entity /#4

--- first/src/main/java/graduation/first/user/User.java
+++ first/src/main/java/graduation/first/user/User.java
@@ -1,9 +1,14 @@
 package graduation.first.user;
 
+import lombok.Getter;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
+@Getter
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
