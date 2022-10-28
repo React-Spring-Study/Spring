7a9583f8777506ef256dba146aa2076f89b998ba
NamJihyun99 <namjihyun9936@gmail.com>
2022-10-28 18:48:54 +0900

    feat:> develop readOnePost api controller /#4

--- first/src/main/java/graduation/first/post/PostController.java
+++ first/src/main/java/graduation/first/post/PostController.java
@@ -1,9 +1,7 @@
 package graduation.first.post;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,5 +12,10 @@ public class PostController {
     @PostMapping("/post/new")
     public Long createPost(@RequestBody PostSaveRequestDto saveDto) {
         return postService.savePost(saveDto);
+    }
+
+    @GetMapping("/post/{postId}")
+    public PostResponseDto readOnePost(@PathVariable Long postId) {
+        return postService.readOnePost(postId);
     }
 }
