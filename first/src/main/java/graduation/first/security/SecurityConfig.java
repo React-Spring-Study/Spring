304c0dca27ae7696f6ed3be6e89c577995a21800
NamJihyun99 <namjihyun9936@gmail.com>
2022-10-31 13:20:50 +0900

    security init /#7

---
+++ first/src/main/java/graduation/first/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package graduation.first.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    public final DefaultOAuth2UserService defaultOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers("/post/**").authenticated()
+                .anyRequest().permitAll()
+
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(defaultOAuth2UserService);
+
+    }
+}
