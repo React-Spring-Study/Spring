d548680b13cdae1daa3f64028fae02e598c3e324
NamJihyun99 <namjihyun9936@gmail.com>
2022-11-05 14:03:04 +0900

    feat:> UserRefreshToken Repository /#7

---
+++ first/src/main/java/graduation/first/user/UserRefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package graduation.first.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
+
+    UserRefreshToken findByUserEmail();
+    UserRefreshToken findByUserEmailAndRefreshToken();
+}
