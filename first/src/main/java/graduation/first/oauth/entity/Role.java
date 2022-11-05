18d2a12f00be8aa472853011ba273fdd41f02762
NamJihyun99 <namjihyun9936@gmail.com>
2022-11-05 13:44:06 +0900

    add:> add admin role /#7

--- first/src/main/java/graduation/first/oauth/entity/Role.java
+++ first/src/main/java/graduation/first/oauth/entity/Role.java
@@ -3,12 +3,22 @@ package graduation.first.oauth.entity;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
+
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST", "손님"),
-    USER("ROLE_USER", "일반 사용자");
+    GUEST("GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자"),
+    ADMIN("ROLE_ADMIN", "관리자");
 
-    private final String key;
+    private final String code;
     private final String title;
+
+    public static Role of(String code) {
+        return Arrays.stream(Role.values())
+                .filter(r -> r.getCode().equals(code))
+                .findAny()
+                .orElse(GUEST);
+    }
 }
