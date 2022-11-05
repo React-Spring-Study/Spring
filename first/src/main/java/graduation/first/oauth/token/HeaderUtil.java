b6c308f6055b46a5cbbae605ed99238d9cb254d0
NamJihyun99 <namjihyun9936@gmail.com>
2022-11-05 13:13:59 +0900

    feat:> develop HeaderUtil /#7

--- first/src/main/java/graduation/first/oauth/token/HeaderUtil.java
+++ first/src/main/java/graduation/first/oauth/token/HeaderUtil.java
@@ -8,6 +8,17 @@ public class HeaderUtil {
     private final static String TOKEN_PREFIX = "Bearer ";
 
     public static String getAccessToken(HttpServletRequest request) {
-        return "";
+
+        String headerValue = request.getHeader(HEADER_AUTHORIZATION);
+
+        if (headerValue == null) {
+            return null;
+        }
+
+        if (headerValue.startsWith(TOKEN_PREFIX)) {
+            return headerValue.substring(TOKEN_PREFIX.length());
+        }
+
+        return null;
     }
 }
