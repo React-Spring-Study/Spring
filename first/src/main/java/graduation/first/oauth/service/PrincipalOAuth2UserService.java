9917e1555a1126d83a2e997d0878b82c6268b31e
NamJihyun99 <namjihyun9936@gmail.com>
2022-11-19 17:28:52 +0900

    feat:>bring userinfo from resource server /#7

---
+++ first/src/main/java/graduation/first/oauth/service/PrincipalOAuth2UserService.java
@@ -0,0 +1,82 @@
+package graduation.first.oauth.service;
+
+import graduation.first.oauth.entity.Provider;
+import graduation.first.oauth.entity.Role;
+import graduation.first.oauth.entity.UserPrincipal;
+import graduation.first.oauth.info.OAuth2UserInfo;
+import graduation.first.oauth.info.OAuth2UserInfoFactory;
+import graduation.first.user.User;
+import graduation.first.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        System.out.println("access token = " + userRequest.getAccessToken().getTokenValue());
+
+        String providerId = oAuth2User.getAttribute("sub");
+        String username = (String) oAuth2User.getAttribute("family_name") + oAuth2User.getAttribute("given_name");
+        String email = oAuth2User.getAttribute("email");
+        String profileImg = oAuth2User.getAttribute("picture");
+
+        return null;
+
+    }
+
+    private OAuth2User process(OAuth2UserRequest request, OAuth2User user) {
+        Provider provider = Provider.valueOf(request.getClientRegistration().getClientId().toUpperCase());
+
+        OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(provider, user.getAttributes());
+        User savedUser = userRepository.findByProviderId(userInfo.getId());
+
+        if (savedUser != null) {
+            if (savedUser.getProvider() != provider)
+                throw new RuntimeException();
+            updateUser(savedUser, userInfo);
+        } else {
+            savedUser = createUser(userInfo, provider);
+        }
+
+        return UserPrincipal.create(savedUser, user.getAttributes());
+    }
+
+    private User createUser(OAuth2UserInfo userInfo, Provider provider) {
+
+        User newUser = User.builder()
+                .name(userInfo.getName())
+                .email(userInfo.getEmail())
+                .profileImg(userInfo.getImgUrl())
+                .provider(provider)
+                .providerId(userInfo.getId())
+                .role(Role.USER)
+                .build();
+
+        return userRepository.save(newUser);
+    }
+
+    private User updateUser(User user, OAuth2UserInfo userInfo) {
+        if (user.getName() != null && !user.getName().equals(userInfo.getName())) {
+            //TODO: change user's name
+        }
+        if (user.getProfileImg() != null && !user.getProfileImg().equals(userInfo.getImgUrl())) {
+            //TODO: change user's profileImgUrl
+        }
+
+        return user;
+    }
+}
--- first/src/main/java/graduation/first/security/SecurityConfig.java
+++ first/src/main/java/graduation/first/security/SecurityConfig.java
@@ -1,12 +1,12 @@
 package graduation.first.security;
 
+import graduation.first.oauth.service.PrincipalOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity
