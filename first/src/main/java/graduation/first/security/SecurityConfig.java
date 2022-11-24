fa95ba931a42de94cd7221490c1556637b6c4100
NamJihyun99 <namjihyun9936@gmail.com>
2022-11-24 13:09:01 +0900

    feat:> develop SecurityConfig /#7

--- first/src/main/java/graduation/first/security/SecurityConfig.java
+++ first/src/main/java/graduation/first/security/SecurityConfig.java
@@ -1,12 +1,37 @@
 package graduation.first.security;
 
+import graduation.first.common.config.AppProperties;
+import graduation.first.common.config.CorsProperties;
+import graduation.first.oauth.entity.Role;
+import graduation.first.oauth.exception.RestAuthenticationEntryPoint;
+import graduation.first.oauth.handler.OAuth2AuthenticationFailureHandler;
+import graduation.first.oauth.handler.OAuth2AuthenticationSuccessHandler;
+import graduation.first.oauth.handler.TokenAccessDeniedHandler;
+import graduation.first.oauth.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
 import graduation.first.oauth.service.PrincipalOAuth2UserService;
+import graduation.first.oauth.service.PrincipalUserDetailsService;
+import graduation.first.oauth.token.AuthTokenProvider;
+import graduation.first.oauth.token.TokenAuthenticationFilter;
+import graduation.first.user.UserRefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -14,19 +39,115 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public final PrincipalOAuth2UserService principalOAuth2UserService;
+    private final CorsProperties corsProperties;
+    private final AppProperties appProperties;
+    private final AuthTokenProvider tokenProvider;
+    private final PrincipalUserDetailsService userDetailsService;
+    private final PrincipalOAuth2UserService oAuth2UserService;
+    private final TokenAccessDeniedHandler tokenAccessDeniedHandler;
+    private final UserRefreshTokenRepository userRefreshTokenRepository;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().headers().frameOptions().disable();
-        http.authorizeRequests()
-                .antMatchers("/post/**").hasRole("USER")
-                .anyRequest().permitAll()
+//        http.csrf().disable().headers().frameOptions().disable();
+
+        http.cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+                .accessDeniedHandler(tokenAccessDeniedHandler)
+
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers("/post/**").hasAnyAuthority(Role.USER.getCode())
+                //TODO: uri 별 권한 추가
+                .anyRequest().authenticated()
 
                 .and()
                 .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorization")
+                .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+
+                .and()
+                .redirectionEndpoint()
+                .baseUri("/*/oauth2/code/*")
+
+                .and()
                 .userInfoEndpoint()
-                .userService(principalOAuth2UserService);
+                .userService(oAuth2UserService)
+
+                .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler())
+                .failureHandler(oAuth2AuthenticationFailureHandler());
+
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler() {
+        return new OAuth2AuthenticationFailureHandler(oAuth2AuthorizationRequestBasedOnCookieRepository());
+    }
+
+    @Bean
+    public OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler() {
+        return new OAuth2AuthenticationSuccessHandler(
+                tokenProvider,
+                appProperties,
+                userRefreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository());
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedHeaders(Arrays.asList(corsProperties.getAllowedHeaders().split(",")));
+        corsConfig.setAllowedMethods(Arrays.asList(corsProperties.getAllowedMethods().split(",")));
+        corsConfig.setAllowedOrigins(Arrays.asList(corsProperties.getAllowedOrigins().split(",")));
+        corsConfig.setMaxAge(corsProperties.getMaxAge());
+
+        corsConfigurationSource.registerCorsConfiguration("/**", corsConfig);
+        return corsConfigurationSource;
+    }
+
 }
