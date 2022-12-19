c2aa32c32837854b106fa9d0a4aeb8bed77bef7e
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-20 01:10:17 +0900

    feat:> userController /#7

---
+++ first/src/main/java/graduation/first/user/UserService.java
@@ -0,0 +1,14 @@
+package graduation.first.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User getUser(String userId) {
+        return userRepository.findByUserId(userId);
+    }
+}
