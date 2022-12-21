01711523d15d673bc80c30fbf4a35adabbbec8f2
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-22 00:04:50 +0900

    fix:> PostController requestmapping /#15

--- first/src/main/java/graduation/first/post/PostController.java
+++ first/src/main/java/graduation/first/post/PostController.java
@@ -4,29 +4,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
-    @PostMapping("/post/new")
+    @PostMapping
     public String createPost(@RequestBody PostSaveRequestDto saveDto) {
         postService.savePost(saveDto);
         return "게시물 저장 성공";
     }
 
-    @GetMapping("/post/{postId}")
+    @GetMapping("/{postId}")
     public PostResponseDto readOnePost(@PathVariable Long postId) {
         return postService.readOnePost(postId);
     }
 
-    @PutMapping("/post/{postId}")
+    @PutMapping("/{postId}")
     public String updatePost(@PathVariable Long postId, @RequestBody PostUpdateRequestDto updateDto) {
         postService.updatePost(postId, updateDto);
         return "게시물 수정 성공";
     }
 
-    @DeleteMapping("/post/{postId}")
+    @DeleteMapping("/{postId}")
     public String deletePost(@PathVariable Long postId) {
         postService.deletePost(postId);
         return "게시물 삭제 성공";
