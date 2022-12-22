d5277581da409d8cc2f3dbd382728963c586925b
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-22 17:20:13 +0900

    feat:> userException handling /#21

---
+++ first/src/main/java/graduation/first/user/UserException.java
@@ -0,0 +1,19 @@
+package graduation.first.user;
+
+import lombok.Getter;
+
+@Getter
+public class UserException extends RuntimeException {
+
+    private UserErrorCode errorCode;
+    private String errorMessage;
+
+    public UserException(UserErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public UserException(UserErrorCode errorCode, String errorMessage) {
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+    }
+}
---
+++ first/src/main/java/graduation/first/user/UserExceptionHandler.java
@@ -0,0 +1,23 @@
+package graduation.first.user;
+
+import graduation.first.common.response.ErrorEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class UserExceptionHandler {
+
+
+    @ExceptionHandler(UserException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorEntity userException(UserException ex) {
+        log.error("User Exception({}) - {}", ex.getErrorCode(), ex.getErrorMessage());
+        return new ErrorEntity(ex.getErrorCode().toString(), ex.getErrorMessage());
+    }
+}
