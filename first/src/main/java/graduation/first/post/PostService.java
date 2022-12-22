3eb2a4a8c6d42434307a2e7b07ee3f82a824ddb4
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-22 19:32:12 +0900

    refactor:> remove writer field in PostUpdateDto /#27

--- first/src/main/java/graduation/first/post/PostService.java
+++ first/src/main/java/graduation/first/post/PostService.java
@@ -14,6 +14,7 @@ import graduation.first.post.exception.PostException;
 import graduation.first.user.domain.User;
 import graduation.first.user.exception.UserErrorCode;
 import graduation.first.user.exception.UserException;
+import graduation.first.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,23 +27,20 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public Long savePost(UserAdapter userAdapter, PostSaveRequestDto saveDto) {
         User writer = userAdapter.getUser();
         Category category = categoryRepository.findByName(saveDto.getCategoryName())
                 .orElseThrow(() -> new PostException(PostErrorCode.CATEGORY_NOT_FOUND));
-
         Post post = Post.builder()
                 .title(saveDto.getTitle())
                 .content(saveDto.getContent())
                 .category(category)
                 .writer(writer)
                 .build();
-
-        Post saveOne = postRepository.save(post);
-
-        return saveOne.getId();
+        return postRepository.save(post).getId();
     }
 
     @Transactional
@@ -55,40 +53,41 @@ public class PostService {
     public Page<PostResponseVO> readPostsByCategory(String categoryName, Pageable pageable) {
         Category category = categoryRepository.findByName(categoryName)
                 .orElseThrow(() -> new CategoryException(CategoryErrorCode.CATEGORY_NOT_FOUND));
-        Page<Post> postList = postRepository.findAllByCategory(category, pageable);
-        return PostResponseVO.toVoList(postList);
+        return PostResponseVO.toVoList(postRepository.findAllByCategory(category, pageable));
     }
 
     @Transactional
     public PostResponseDto readOnePost(Long postId) {
-        Post findOne = postRepository.findById(postId)
-                .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
-        return PostResponseDto.toDto(findOne);
+        return PostResponseDto.toDto(getPostById(postId));
     }
 
     @Transactional
     public Long updatePost(UserAdapter userAdapter,
                            Long postId,
                            PostUpdateRequestDto updateDto) {
-        checkWriterAuth(userAdapter, updateDto.getWriter());
-        Post findOne = postRepository.findById(postId)
-                .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
+        Post post = getPostById(postId);
+        checkWriterAuth(userAdapter, post.getWriter().getId());
         Category category = categoryRepository.findByName(updateDto.getCategoryName())
                 .orElseThrow(() -> new CategoryException(CategoryErrorCode.CATEGORY_NOT_FOUND));
-        findOne.update(updateDto.getTitle(), updateDto.getContent(), category);
+        post.update(updateDto.getTitle(), updateDto.getContent(), category);
         return postId;
     }
 
     @Transactional
     public void deletePost(UserAdapter userAdapter, Long postId) {
-        Post findOne = postRepository.findById(postId)
-                .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
-        checkWriterAuth(userAdapter, findOne.getWriter());
+        Post findOne = getPostById(postId);
+        checkWriterAuth(userAdapter, findOne.getWriter().getId());
         postRepository.delete(findOne);
     }
 
-    private void checkWriterAuth(UserAdapter userAdapter, User writer) {
-        if(userAdapter.getUser()!= writer)
+    private Post getPostById(Long postId) {
+        Post findOne = postRepository.findById(postId)
+                .orElseThrow(() -> new PostException(PostErrorCode.POST_NOT_FOUND));
+        return findOne;
+    }
+
+    private void checkWriterAuth(UserAdapter userAdapter, Long writerId) {
+        if(userAdapter.getUser().getId()!= writerId)
             throw new UserException(UserErrorCode.USER_NOT_PERMITTED);
     }
 }
