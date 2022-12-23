9fbc58589c3d49ba4ce03d3674a888a9f521b211
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-24 02:31:03 +0900

    deploy:> switch.sh /deploy

--- first/scripts/run_new_was.sh
+++ first/scripts/run_new_was.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_PORT=$(cat /home/ec2-user/service-url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 echo "> Current Port of running WAS is ${CURRENT_PORT}."
