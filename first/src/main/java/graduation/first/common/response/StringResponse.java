e9120fefaa0d419ff532df02a18438b4b295cc09
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-26 05:20:29 +0900

    refactor:> add OnlyStringResponse /#53

---
+++ first/src/main/java/graduation/first/common/response/StringResponse.java
@@ -0,0 +1,12 @@
+package graduation.first.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StringResponse {
+    private String message;
+}
--- first/src/main/java/graduation/first/post/PostController.java
+++ first/src/main/java/graduation/first/post/PostController.java
@@ -1,5 +1,6 @@
 package graduation.first.post;
 
+import graduation.first.common.response.StringResponse;
 import graduation.first.oauth.entity.UserAdapter;
 import graduation.first.post.dto.PostResponseDto;
 import graduation.first.post.dto.PostResponseVO;
@@ -21,9 +22,9 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public String createPost(@AuthenticationPrincipal UserAdapter user, @Valid @RequestBody PostSaveRequestDto saveDto) {
+    public StringResponse createPost(@AuthenticationPrincipal UserAdapter user, @Valid @RequestBody PostSaveRequestDto saveDto) {
         postService.savePost(user, saveDto);
-        return "게시물 저장 성공";
+        return new StringResponse("게시물 저장 성공");
     }
 
     @GetMapping
@@ -42,16 +43,16 @@ public class PostController {
     }
 
     @PutMapping("/{postId}")
-    public String updatePost(@AuthenticationPrincipal UserAdapter user,
+    public StringResponse updatePost(@AuthenticationPrincipal UserAdapter user,
                              @PathVariable Long postId,
                              @Valid @RequestBody PostUpdateRequestDto updateDto) {
         postService.updatePost(user, postId, updateDto);
-        return "게시물 수정 성공";
+        return new StringResponse("게시물 수정 성공");
     }
 
     @DeleteMapping("/{postId}")
-    public String deletePost(@AuthenticationPrincipal UserAdapter user, @PathVariable Long postId) {
+    public StringResponse deletePost(@AuthenticationPrincipal UserAdapter user, @PathVariable Long postId) {
         postService.deletePost(user, postId);
-        return "게시물 삭제 성공";
+        return new StringResponse("게시물 삭제 성공");
     }
 }
