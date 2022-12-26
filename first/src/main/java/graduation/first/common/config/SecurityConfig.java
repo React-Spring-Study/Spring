6e8329dd854552fdf67b6d2721f75720932164d7
NamJihyun99 <namjihyun9936@gmail.com>
2022-12-26 14:01:51 +0900

    test:> retry change port /#53

--- first/src/main/java/graduation/first/common/config/SecurityConfig.java
+++ first/src/main/java/graduation/first/common/config/SecurityConfig.java
@@ -72,8 +72,9 @@ public class SecurityConfig {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/health").permitAll()
+                .antMatchers(HttpMethod.GET, "/v1/posts").permitAll()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .antMatchers("/v1/**").hasAnyAuthority(Role.USER.getCode())
+                .antMatchers("/v1/users", "/v1/auth").hasAnyAuthority(Role.USER.getCode())
                 //TODO: uri 별 권한 추가
                 .anyRequest().authenticated()
 
