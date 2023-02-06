c001f46da611313fa0695056b354590b95d2baf6
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-06 14:41:50 +0900

    fix:> add noArgsConstructor for tokenReissueRequest/#8

--- first/src/main/java/graduation/first/oauth/token/TokenReissueRequest.java
+++ first/src/main/java/graduation/first/oauth/token/TokenReissueRequest.java
@@ -1,10 +1,13 @@
 package graduation.first.oauth.token;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TokenReissueRequest {
     String accessToken;
 }
