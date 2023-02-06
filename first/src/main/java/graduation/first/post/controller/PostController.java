c9169ebaad7e81818f205756ec28497a6ab33d43
NamJihyun99 <namjihyun9936@gmail.com>
2023-02-06 14:53:26 +0900

    fix:> fix findLoggingInUser/#8

--- first/src/main/java/graduation/first/post/controller/PostController.java
+++ first/src/main/java/graduation/first/post/controller/PostController.java
@@ -68,7 +68,7 @@ public class PostController {
 
     private User findLoggingInUser() {
 //        org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        UserPrincipal principal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication();
+        UserPrincipal principal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userService.getUser(principal.getUsername());
         return user;
     }
